4fcc3f8a149b5c58e9bec1b25f5eb2aeaee36e1d
Tim Snow <tim.snow1991@gmail.com>
2023-01-16 22:33:24 +0000

    feat: new day rust snippet, fix eslint&prettier config issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,16 +27,20 @@ module.exports = {
     semi: [2, 'always'],
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/space-before-function-paren': 0,
-    '@typescript-eslint/member-delimiter-style': {
-      multiline: {
-        delimiter: 'semi',
-        requireLast: true,
+    '@typescript-eslint/semi': 0,
+    '@typescript-eslint/member-delimiter-style': [
+      2,
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        multilineDetection: 'brackets',
       },
-      singleline: {
-        delimiter: 'semi',
-        requireLast: false,
-      },
-      multilineDetection: 'brackets',
-    },
+    ],
   },
 };
