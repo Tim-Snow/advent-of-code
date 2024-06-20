5fb63630073c4b85c2141d8d456f0d7127f859c1
Tim Snow <tim.snow1991@gmail.com>
2024-06-20 22:46:56 +0100

    feat: measure execution time

--- src/y2015/day1.go
+++ src/y2015/day1.go
@@ -3,6 +3,7 @@ package y2015
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func check(e error) {
@@ -17,6 +18,14 @@ func assert[T comparable](actual T, expected T) {
 	}
 }
 
+func track() time.Time {
+	return time.Now()
+}
+
+func duration(start time.Time) {
+	fmt.Printf("Execution time: %v\n", time.Since(start))
+}
+
 func Day1() {
 	test_data, err := os.ReadFile("../res/2015/1.test.txt")
 	check(err)
@@ -25,10 +34,13 @@ func Day1() {
 
 	p1 := part1(test_data)
 	assert(p1, 3)
-	fmt.Printf("Part 1: %d\n", part1(data))
 
 	p2 := part2(test_data)
 	assert(p2, 1)
+
+	defer duration(track())
+
+	fmt.Printf("Part 1: %d\n", part1(data))
 	fmt.Printf("Part 2: %d\n", part2(data))
 }
 
